9f122f00ac695e12e63c737e158620adfd85c7bb
Olivier Binette <olivier.binette@gmail.com>
2021-01-14 16:39:57 -0500

    Add content

---
+++ R/ocr-utils.R
@@ -0,0 +1,115 @@
+#' Runs tesseract-OCR on the given image files.
+#'
+#' This function performs a system call to the tesseract command line tool, running tesseract-OCR on the given images.
+#'
+#' @usage hocr_from_images(imgfiles, outputdir=here(), silent=FALSE, options="")
+#'
+#' @param imgfiles list of image file paths. Image format should be compatible with tesseract as configured on the user's system. JPEG and PNG files are generally compatible.
+#' @param outputdir directory where to store the output hocr files.
+#' @param silent whether or not to supress messages (default is FALSE).
+#' @param options additional options to pass to the tesseract command line tool. E.g. options="--psm 1" will use page segmentation mode one.
+#'
+#' @return List of output hocr file paths.
+#'
+#' @import parallel here
+#' @export
+hocr_from_images <- function(imgfiles, outputdir=here(), silent=FALSE, options="") {
+
+  imgnames = basename(tools::file_path_sans_ext(imgfiles))
+  n = length(imgfiles)
+
+  ncores = min(n, parallel::detectCores())
+  if (!silent) {
+    cat(crayon::green("Running tesseract-OCR on", length(imgfiles), "image files.\n"))
+  }
+  parallel::mclapply(1:n, function(i) {
+    cmd = paste("tesseract",
+                imgfiles[[i]],
+                file.path(outputdir, imgnames[[i]]),
+                "hocr",
+                options)
+    system(cmd, wait=TRUE, ignore.stdout=silent)
+  }, mc.cores = parallel::detectCores(), mc.preschedule = FALSE)
+
+  return(file.path(outputdir, paste0(imgnames, ".hocr")))
+}
+
+#' Run tesseract OCR on a zipped set of images.
+#'
+#' This function unzips images and performs a system call to the tesseract command line tool, running tesseract-OCR on the given images.
+#'
+#' @param zipped path to zip file containing images.
+#' @param outputdir directory where to store the output hocr files.
+#' @param exdir directory where images are extracted to. Set to NULL to extract image in a temporary folder.
+#' @param silent whether or not to supress messages (default is FALSE).
+#' @param options additional options to pass to the tesseract command line tool. E.g. options="--psm 1" will use page segmentation mode one.
+#'
+#' @return List of output hocr file paths.
+#'
+#' @import tools here crayon parallel assert utils
+#' @export
+hocr_from_zip <- function(zipped, outputdir=".", exdir=NULL, silent=FALSE, options="") {
+  assert(file.exists(zipped))
+
+  if (is.null(exdir)) {
+    exdir = file.path(tempdir(), "exdir")
+    if (!dir.exists(exdir)) dir.create(exdir)
+    purge = TRUE
+  } else purge = FALSE
+
+  imgfiles = unzip(zipped, exdir=exdir)
+  res = hocr_from_images(imgfiles, outputdir=outputdir, silent=silent, options=options)
+
+  if (purge) unlink(exdir, recursive=TRUE)
+
+  return(res)
+}
+
+#' In-browser visualization of hocr files
+#'
+#' This function creates an html file which can be opened in the browser and which showcases the hocr output using hocrjs.
+#'
+#' @usage visualize_html(hocr_file, outputdir=".")
+#'
+#' @param hocr_file hocr file path to visualize.
+#' @param outputdir directory where to store the viewable html. This html file has the same name as the hocr file and can be opened in any browser.
+#'
+#' @import readr stringi
+#' @export
+visualize_html <- function(hocr_file, outputdir=".") {
+  assert(file.exists(hocr_file))
+  filename = paste0(basename(tools::file_path_sans_ext(hocr_file)), ".html")
+
+  txt = readr::read_file(hocr_file)
+  txt = stringi::stri_replace(txt, '<script src="https://unpkg.com/hocrjs"></script></body>', regex='</body>')
+
+  cat(txt, file=file.path(outputdir, filename))
+}
+
+#' Extract text paragraphs and bounding boxes from hocr file.
+#'
+#' Contructs a dataframe containing paragraphs and bounding boxes from an hocr file created by tesseract-OCR.
+#'
+#' @usage paragraphs(hocr_file)
+#'
+#' @param hocr_file path to hocr file.
+#'
+#' @return dataframe with columns "bbox1", "bbox2", "bbox3", "bbox4" and "text" for the four corners of the paragraph bounding box and the text content.
+#'
+#' @import xml2 rvest stringr stringi dplyr purrr
+#' @export
+paragraphs <- function(hocr_file) {
+  hocr_file %>%
+    xml2::read_html() %>%
+    rvest::html_nodes(".ocr_par") %>%
+    purrr::map_dfr(function(x) {
+      c(bbox = strsplit(html_attrs(x)[["title"]], " ")[[1]][2:5],
+        text = x %>%
+          rvest::html_text() %>%
+          stringi::stri_replace_all("", fixed="\n") %>%
+          stringi::stri_replace_all("", fixed="- ") %>%
+          stringr::str_squish())
+    })
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# historical-newspaper-analysis
-Analysis of historical newspaper archives using Tesseract OCR and Entity Resolution for article extraction.
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# TessTools: Tools for the use of Tesseract OCR in R
+
+![Lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)
+
+Interface to the Tesseract OCR command line tool and parsing functions.
+This is under development.
+
+## Installation
+
+You can install the development version from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("OlivierBinette/TessTools")
+```
+
+## Example
+
+Run Tesseract OCR on newspaper scans. Extract paragraph text and
+bounding boxes.
+
+``` r
+library(TessTools)
+#> Warning: replacing previous import 'readr::guess_encoding' by
+#> 'rvest::guess_encoding' when loading 'TessTools'
+#> Warning: replacing previous import 'purrr::pluck' by 'rvest::pluck' when loading
+#> 'TessTools'
+
+# Run Tesseract OCR and get hocr output file paths.
+outputfiles = hocr_from_zip("data-raw/dchnp71001.zip", outputdir="data-raw")
+#> Running tesseract-OCR on 4 image files.
+
+# Extract paragraph text from the first page.
+text = paragraphs(outputfiles[[1]])
+tail(text)
+#> # A tibble: 6 x 5
+#>   bbox1 bbox2 bbox3 bbox4 text                                                  
+#>   <chr> <chr> <chr> <chr> <chr>                                                 
+#> 1 4001  5810  4872  6309  "ural forces to do the work which we would make cheap…
+#> 2 4221  6182  4872  6455  "forces of our we under I answer unhesitatingly,"     
+#> 3 4006  6327  4874  6729  "labor, which should take? educate and train our own …
+#> 4 4008  6770  4924  7554  "At Whitney there is being .de veloped a water power …
+#> 5 4159  7573  4725  7621  "(Continued on third page.)"                          
+#> 6 0     0     5150  8000  ""
+```
